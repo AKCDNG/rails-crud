bbedb741f49b59ae55f3bb4ad728f99a58896a89
Anish <anishkakaiya@outlook.com>
2022-08-01 14:50:45 +0100

    fix test syntax

--- spec/requests/about_me_spec.rb
+++ spec/requests/about_me_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "About", type: :request do
   it "returns http success" do
-    get "/about"
+    get "/home/about"
     expect(response).to have_http_status(:success)
   end
 end
