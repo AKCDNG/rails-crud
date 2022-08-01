1c194165b7ec539f8da66ed5d1a3dc9bbbcd98a7
Anish <anishkakaiya@outlook.com>
2022-08-01 14:43:45 +0100

    fix test

--- spec/requests/about_me_spec.rb
+++ spec/requests/about_me_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "About", type: request do
+RSpec.describe "About", type: :request do
   it "returns http success" do
     get "/about"
     expect(response).to have_http_status(:success)
